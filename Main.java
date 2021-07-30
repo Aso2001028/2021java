fde2d18111cb63b3a7f656d9bc907e358c3e6891
Aso2001028 <83046903+Aso2001028@users.noreply.github.com>
2021-07-30 21:47:37 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        System.out.println("戦艦ゲーム");
+
+        int turn =1;
+        int count =1;
+        while(count != 0 && turn<10){
+            System.out.printf("%sターン目\n",turn++);
+            System.out.println("爆弾のX座標を指定してください");
+            int x = new java.util.Scanner(System.in).nextInt();
+            System.out.println("爆弾のY座標を指定してください");
+            int y = new java.util.Scanner(System.in).nextInt();
+
+             
+            }
+        }
+    }
+
